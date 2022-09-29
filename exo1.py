6cf41b0af600da2aac9e0096005096fd9a3915b9
ShilkovKonst <konst.shilkov@gmail.com>
2022-09-29 12:42:59 +0200

    fix

--- exo1.py
+++ exo1.py
@@ -1,9 +1,9 @@
 a=int(input('Enter any int number lesser than 4000: '))
-if (0<a<4000):
+if (0<a<40000):
     b=int(input('Enter another int number lesser than 4000: '))
-    if (0<b<4000):
+    if (0<b<40000):
         c=int(input('Enter another int number lesser than 4000: '))
-        if (0<b<4000):
+        if (0<b<40000):
             print((a+b+c)/3)
         else: 
             print('wrong number!!!!')
